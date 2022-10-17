5dc13ca9e3d701b0351d67eaf381e4a5e412d16b
Denys Milus <nabajiubass2000@gmail.com>
2022-10-17 11:04:30 +0300

    hide firebase configs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2OsKWAmI8pMh9_Ly7eu6KH8TDn-3DGkY",
-  authDomain: "vue3-webrtc.firebaseapp.com",
-  projectId: "vue3-webrtc",
-  storageBucket: "vue3-webrtc.appspot.com",
-  messagingSenderId: "879622077137",
-  appId: "1:879622077137:web:6bb5a6d289d1c9eb4663f0"
+  // firebase config
 };
 
 const app = firebase.initializeApp(firebaseConfig);
